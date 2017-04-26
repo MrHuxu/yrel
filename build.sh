230aee3d19a9d22a44e5a4ec47888425118cb056
xhu <xhu@freewheel.tv>
2017-04-26 17:57:54 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+# go get golang.org/x/tools/cmd/goyacc
+
 # use the yacc .y file to generate parser
-go tool yacc -o parser/parser.go parser/parser.y
+goyacc -o parser/parser.go parser/parser.y
 
 # change first letter to upper case for export the function
 sed -i '' "s/yyParse/YyParse/g" `grep yyParse parser/ -rl`
